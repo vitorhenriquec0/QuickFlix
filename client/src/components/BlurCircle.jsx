73dbf5f4e73a9a13178d4ddb36d0ae4f7ed9008d
vitorhenriquec0 <vitorhenriquecardoso59@gmail.com>
2026-05-31 17:43:21 -0300

    first commit

---
+++ client/src/components/BlurCircle.jsx
@@ -0,0 +1,12 @@
+
+const BlurCircle = ({top = "auto", left = "auto", right = "auto", bottom = "auto"}) => {
+  return (
+    <div className="absolute -z-50 h-58 x-58
+    aspect-square rounded-full bg-primary/30
+    blur-3xl" style={{top: top, right: right, left: left, bottom: bottom}}>
+        
+    </div>
+  )
+}
+
+export default BlurCircle
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturedSelection from '../components/FeaturedSection'
+
+const Home = () => {
+  return (
+    <>
+        <HeroSection />
+        <FeaturedSelection />
+    </>
+  )
+}
+
+export default Home
