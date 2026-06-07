8cc0f5cecbbb931133f90bd7b480b422614e4526
vitorhenriquec0 <vitorhenriquecardoso59@gmail.com>
2026-06-07 15:47:02 -0300

    project completed

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg'
-import marvelLogo from './marvelLogo.svg'
+import a24logo from './a24logo.png'
 import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
 import screenImage from './screenImage.svg'
@@ -7,7 +7,7 @@ import profile from './profile.png'
 
 export const assets = {
     logo,
-    marvelLogo,
+    a24logo,
     googlePlay,
     appStore,
     screenImage,
--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -8,26 +8,24 @@ const HeroSection = () => {
     const navigate = useNavigate()
 
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("./assets/backgroundImage.png")] 
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("./assets/backgroundImage.webp")] 
     bg-cover bg-center h-screen'>
-        <img src={assets.marvelLogo} alt="" className='max-h-11 lg:h-11 mt-20' />
-
-        <h1 className='text-5x1 md:text-[70px] md:leading-18 font-semibold max-w-110'>Guardians <br/> of the Galaxy</h1>
+        
+        {/* <img src={assets.a24logo} alt="" className='max-h-11 lg:h-11 mt-20' /> */}
+        
+        <h1 className='text-5x1 md:text-[70px] md:leading-18 font-semibold max-w-110'>Obsession</h1>
 
         <div className='flex items-center gap-4 text-gray-300'>
-            <span>Action | Adventure | Sci-Fi</span>
+            <span>Horror | Triller</span>
             <div className='flex items-center gap-1'>
-                <CalendarIcon className='w-4.5 h-4.5' /> 2018
+                <CalendarIcon className='w-4.5 h-4.5' /> 2026
             </div>
             <div className='flex items-center gap-1'>
-                <ClockIcon className='w-4.5 h-4.5' /> 2h 8m
+                <ClockIcon className='w-4.5 h-4.5' /> 1h 40m
             </div>
         </div>
 
-        <p className='max-w-md text-gray-300'>In a post-apocalyptic world where cities ride on wheels and
-            consume each other to survive, two people meet in London and try to stop
-            a conspiracy.
-        </p>
+        <p className='max-w-md text-gray-300'>After breaking the mysterious "One Wish Willow" to win his crush's heart, a hopeless romantic finds himself getting exactly what he asked for but soon discovers that some desires come at a dark, sinister price.</p>
         <button onClick={()=> navigate('/movies')} className='flex items-center gap-1 px-6 py-3 text-sm bg-primary
         hover:bg-pramary-dull transition rounded-full font-medium cursor-pointer'>
             Explore Movies
