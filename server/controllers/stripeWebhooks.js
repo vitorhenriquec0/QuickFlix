7f7f269b12f9faa325481ef681e16c8eeeec00f0
vitorhenriquec0 <vitorhenriquecardoso59@gmail.com>
2026-06-07 13:52:40 -0300

    booking confirmation email

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -1,5 +1,6 @@
 import stripe from "stripe";
 import Booking from "../models/Booking.js";
+import { inngest } from "../inngest/index.js";
 
 export const stripeWebhooks = async (request, response) => {
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
@@ -27,6 +28,12 @@ export const stripeWebhooks = async (request, response) => {
                 await Booking.findByIdAndUpdate(bookingId, {
                     isPaid: true,
                     paymentLink: ""
+                })
+
+                // Send Confirmation Email
+                await inngest.send({
+                    name: "app/show.booked",
+                    data: {bookingId}
                 })
 
                 break;
